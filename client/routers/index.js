6ea647f9145929e9fb74f9f76a55e32b7178d7f8
zhanglun <zhanglun1410@gmail.com>
2017-12-17 18:05:38 +0800

    add tango-editor

--- client/routers/index.js
+++ client/routers/index.js
@@ -1,10 +1,16 @@
 import VueRouter  from 'vue-router';
 import AppView from '../app.vue';
 
+import TangoView from '../pages/tango/index.vue';
+
 const routes = [
   {
     path: '/',
     component: AppView,
+  },
+  {
+    path: '/tango',
+    component: TangoView,
   },
 ];
 
--- tools/webpack.base.config.js
+++ tools/webpack.base.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const AssetsJSONPlugin = require('assets-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const hotMiddlewareScript = 'webpack-hot-middleware/client?reload=true';
 const publicPath = 'http://localhost:3000/';
 
@@ -20,7 +21,8 @@ module.exports = {
         loader: 'babel-loader',
         query: {
           presets: 'es2015',
-        }
+        },
+        exclude: '/node_modules/',
       },
       {
         enforce: 'pre',
@@ -28,19 +30,25 @@ module.exports = {
         loader: 'eslint-loader',
         exclude: [path.resolve(__dirname, '../node_modules/')],
       },
-      // but use vue-loader for all *.vue files
       {
         test: /\.vue$/,
         loader: 'vue-loader',
-        include: [path.resolve(__dirname, '../client/')],
+        include: [path.resolve(__dirname, '../client/'), 'node_modules/vue-monaco-editor'],
       },
     ]
   },
   resolve: {
     alias: {
       'vue': path.resolve('node_modules', 'vue/dist/vue.js'),
+      // 'vs': path.resolve('node_modules', 'monaco-editor/dev/vs'),
     },
   },
   plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: 'node_modules/monaco-editor/min/vs',
+        to: 'vs',
+      }
+    ])
   ]
 };
