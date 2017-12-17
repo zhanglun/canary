3c22b85d12451405953bd5dc50d286250f370f95
zhanglun <zhanglun1410@gmail.com>
2017-12-17 18:54:10 +0800

    add

---
+++ client/components/tango-editor/loader.js
@@ -0,0 +1,72 @@
+/* https://github.com/matt-oconnell/vue-monaco-editor */
+
+module.exports = {
+  /* For now: default to cdn. */
+  load(srcPath = 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.10.1/min', callback) {
+    if (window.monaco) {
+      callback();
+      return;
+    }
+
+    const config = {
+      paths: {
+        vs: srcPath + '/vs',
+      },
+      // 'vs/nls' : {
+      //   availableLanguages: {
+      //     '*': 'zh-cn'
+      //   },
+      // },
+    };
+
+    const loaderUrl = `${config.paths.vs}/loader.js`;
+    const onGotAmdLoader = () => {
+
+      if (window.LOADER_PENDING) {
+        window.require.config(config);
+      }
+
+      // Load monaco
+      window.require(['vs/editor/editor.main'], () => {
+        callback();
+      });
+
+      // Call the delayed callbacks when AMD loader has been loaded
+      if (window.LOADER_PENDING) {
+        window.LOADER_PENDING = false;
+        const loaderCallbacks = window.LOADER_CALLBACKS;
+        if (loaderCallbacks && loaderCallbacks.length) {
+          let currentCallback = loaderCallbacks.shift();
+          while (currentCallback) {
+            currentCallback.fn.call(currentCallback.window);
+            currentCallback = loaderCallbacks.shift();
+          }
+        }
+      }
+    };
+
+    // Load AMD loader if necessary
+    if (window.LOADER_PENDING) {
+      // We need to avoid loading multiple loader.js when there are multiple editors loading concurrently
+      //  delay to call callbacks except the first one
+      window.LOADER_CALLBACKS = window.LOADER_CALLBACKS || [];
+      window.LOADER_CALLBACKS.push({
+        window: this,
+        fn: onGotAmdLoader
+      });
+    } else {
+      if (typeof window.require === 'undefined') {
+        const loaderScript = window.document.createElement('script');
+
+        loaderScript.type = 'text/javascript';
+        loaderScript.src = loaderUrl;
+        loaderScript.addEventListener('load', onGotAmdLoader);
+
+        window.document.body.appendChild(loaderScript);
+        window.LOADER_PENDING = true;
+      } else {
+        onGotAmdLoader();
+      }
+    }
+  }
+}
---
+++ client/components/test-editor/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<div ref="reference"></div>
+	</div>
+</template>
+
+<script>
+	const loader = require('vs/loader.js');
+
+	console.log(loader);
+
+	export default {
+		mounted() {
+			loader.config({ paths: { 'vs': 'monaco-editor/min/vs' }});
+			loader.require(['vs/editor/editor.main'], function() {
+				var editor = monaco.editor.create(document.getElementById('container'), {
+					value: [
+						'function x() {',
+						'\tconsole.log("Hello world!");',
+						'}'
+					].join('\n'),
+					language: 'javascript'
+				});
+			});
+		},
+	}
+</script>
