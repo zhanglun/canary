1a342dee15ac132930bab53270592de13f2e006c
zhanglun <zhanglun1410@gmail.com>
2018-01-17 23:18:30 +0800

    add config for production

--- server/config/config.js
+++ server/config/config.js
@@ -8,6 +8,15 @@ const config = {
       database: 'music',
     },
   },
+  production: {
+    mysql: {
+      connectionLimit: 10,
+      host: 'localhost',
+      port: '',
+      user: 'root',
+      database: 'music',
+    },
+  },
 };
 
 module.exports = config;
